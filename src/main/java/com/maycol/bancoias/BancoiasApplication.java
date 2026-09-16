71fccb805be2bac1d3f5e02c82b60c12b3215025
Maycol David Sanchez Mora <maycol.sanchez@konecta-group.com>
2026-09-16 10:51:14 -0500

    feat: creación de proyecto java spring con webflux

---
+++ src/main/java/com/maycol/bancoias/BancoiasApplication.java
@@ -0,0 +1,13 @@
+package com.maycol.bancoias;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BancoiasApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(BancoiasApplication.class, args);
+  }
+
+}
